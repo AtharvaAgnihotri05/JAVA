7657073e8180ace01b1df0584f4d4fe256e20dce
Atharva Agnihotri <141016822+AtharvaAgnihotri05@users.noreply.github.com>
2025-01-20 13:50:49 +0000

    java basics

--- Basics/conditional.java
+++ Basics/conditional.java
@@ -10,17 +10,26 @@ public class conditional {
         //     System.out.println("You are not eligible to vote");
         // }
 
-        int amount = 300;
-        if (amount > 100 && amount < 300)
-        {
-            System.out.println("Don't start SIP");
-        }
-        else if (amount > 300 && amount < 500) {
-            System.out.println("Invest in 1 or 2 SIP");
-        }
-        else
-        {
-            System.out.println("Invest in all type of CAP");
-        }
+        // int amount = 300;
+        // if (amount > 100 && amount < 300)
+        // {
+        //     System.out.println("Don't start SIP");
+        // }
+        // else if (amount > 300 && amount < 500) {
+        //     System.out.println("Invest in 1 or 2 SIP");
+        // }
+        // else
+        // {
+        //     System.out.println("Invest in all type of CAP");
+        // }
+
+        //ternary operator
+        int a = 12;
+        int b = 10;
+        int c = 120;
+
+        int max;
+        max = a > b ? a > c ? a : c : b > c ? b : c;   
+        System.out.println("Max value is "+max);
     }
 }
